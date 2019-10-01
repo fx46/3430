14c1825846bf7c451d2aaa3097479bae10cb2c88
fx46 <31199346+fx46@users.noreply.github.com>
2019-10-01 19:39:39 -0400

    Tests AC for function bipartite :bomb:

--- TP2/Graph App/AC_test.py
+++ TP2/Graph App/AC_test.py
@@ -6,6 +6,7 @@ class TestSimpleGraphGeneratorsAC(unittest.TestCase):
     def setUp(self):
         self.simpleGraph = None
         self.simpleGraph_Probility = None
+        self.bipartiteGraph = None
 
     #############################################################################
     #
@@ -150,6 +151,122 @@ class TestSimpleGraphGeneratorsAC(unittest.TestCase):
             bException = True
 
         self.assertTrue(self.simpleGraph_Probility is not None and bException is False)
+
+    #############################################################################
+    #
+    # Bipartite
+    #
+    # V1_1 = {V<0}           	[ValueError]
+    # V1_2 = {V>=0}
+    # V2_1 = {V<0}           	[ValueError]
+    # V2_2 = {V>=0}
+    # E1 = {E<0}           	    [ValueError]
+    # E2 = {E>V1*V2}         	[ValueError]
+    # E3 = {0 <= E <= V1*V2}
+    #
+    #############################################################################
+
+    def test_bipartite_V1_1V2_1E1(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(-1, -1, -1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V2_1V2_1E2(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(-1, -1, 2)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V1_1V2_1E3(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(-1, -1, 1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V1_1V2_2E1(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(-1, 1, -1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V2_1V2_2E2(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(-1, 1, 2)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V1_2V2_1E1(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(1, -1, -1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V2_2V2_1E2(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(1, -1, 2)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V1_2V2_2E1(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(1, 1, -1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V2_2V2_2E2(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(1, 1, 2)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is None and bException is True)
+
+    def test_bipartite_V1_2V2_2E3(self):
+        bException = False
+
+        try:
+            self.bipartiteGraph = generators.bipartite(1, 1, 1)
+        except ValueError:
+            bException = True
+
+        self.assertTrue(self.bipartiteGraph is not None and bException is False)
+
+
 
 if __name__ == '__main__':
     unittest.main()
