e73d4509346b5377147d7f0c9d707429ab74f82c
fx46 <31199346+fx46@users.noreply.github.com>
2019-11-18 00:23:42 -0500

    Queue testing

---
+++ TP5/QueueTest.py
@@ -0,0 +1,37 @@
+import unittest
+
+from app import Queue
+
+class QueueTest(unittest.TestCase):
+
+    def setUp(self):
+        self.queue = Queue(3)
+
+    # Test __init__() 
+    def testInit(self):
+        self.assertEqual(self.queue.max_size, 3)
+
+    # Test __init__() -> Enqueue() -> Dequeue()
+    def testEnqueuDequeue(self):
+
+        self.queue.enqueue("test")
+        self.assertFalse(self.queue.isEmpty())
+        self.assertTrue(self.queue.size() == 1)
+        self.assertEqual(self.queue.check().value, "test")
+        self.assertEqual(self.queue.peek().value, "test")
+        self.assertTrue(self.queue.isEmpty())
+
+        self.queue.enqueue("test1")
+        self.queue.enqueue("test2")
+        self.queue.enqueue("test3")
+        self.assertTrue(self.queue.isFull())
+        self.assertEqual(self.queue.size(), self.queue.max_size)
+        try:
+            self.queue.enqueue("test4")
+		except Exception as ex:
+            self.assertEqual(str(ex), "Queue overflow")
+            
+        self.assertEqual(self.queue.dequeue().value, "test1")
+        self.assertEqual(self.queue.dequeue().value, "test2")
+        self.assertEqual(self.queue.dequeue().value, "test3")
+        self.assertRaises(ValueError, self.queue.dequeue())
--- TP5/Test.py
+++ TP5/Test.py
@@ -2,10 +2,12 @@ import unittest
 
 from NodeTest import NodeTest
 from LinkedListTest import LinkedListTest
+from QueueTest import QueueTest
 
 suite = unittest.TestSuite()
 
 suite.addTest(unittest.makeSuite(NodeTest))
 suite.addTest(unittest.makeSuite(LinkedListTest))
+suite.addTest(unittest.makeSuite(QueueTest))
 
 unittest.TextTestRunner(verbosity=2).run(suite)
